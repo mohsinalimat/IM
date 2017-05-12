7b43ee49b9a65e829abef469a652d48d817a8542
Meniny <meniny@qq.com>
2017-05-13 00:01:25 +0800

    0.0.1 init

---
+++ IM-Example/IM-Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IM-Example
+//
+//  Created by Meniny on 2017-05-12.
+//  Copyright © 2017年 Meniny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
